0f2e16b934d226a24e80d5e063acf4fccc572e84
Pat Nakajima <patnakajima@gmail.com>
2024-09-22 10:15:49 -0700

    use a different polyfill

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,6 @@
-import { on } from "delegated-events";
-import { execute } from "./wasi";
 import "./editor";
 import "./style.css";
-import smoothscroll from "smoothscroll-polyfill";
+import { polyfill } from "seamless-scroll-polyfill";
 
 // kick off the polyfill!
-smoothscroll.polyfill();
-
-on("click", ".js-run", async function (e) {
-  e.preventDefault();
-
-  if (!(e.target instanceof HTMLElement)) {
-    return;
-  }
-
-  const container = e.target.closest(".js-run-container");
-  const code = container?.querySelector(".js-code")?.textContent;
-
-  if (!code) {
-    return;
-  }
-
-  e.target.classList.add("opacity-50");
-
-  const { stdout } = await execute(code);
-
-  e.target.classList.remove("opacity-50");
-
-  const result = container?.querySelector(".js-run-result");
-  if (result) {
-    result.innerHTML = resultOutput(stdout);
-    result.scrollIntoView({ behavior: "smooth", block: "nearest" });
-  }
-});
-
-function resultOutput(stdout: string): string {
-  return `
-    <div class="font-mono mt-3">
-      <h4 class="text-gray-500">Result:</h4>
-      <pre class="bg-gray-800 p-3">${stdout}</pre>
-    </div>
-  `;
-}
+polyfill();
