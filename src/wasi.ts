0f7184b66cb4b1a0014a8df9636de25c0472d1b4
Pat Nakajima <patnakajima@gmail.com>
2024-09-22 13:10:41 -0700

    don't try to fetch the wasm so many times

--- src/wasi.ts
+++ src/wasi.ts
@@ -4,32 +4,34 @@ import { Buffer } from "buffer";
 import { lowerI64Imports } from "@wasmer/wasm-transformer";
 import { init, WASI } from "@wasmer/wasi";
 
-// Global variable to store the compiled WebAssembly module
-let talk: WebAssembly.Module | null = null;
+// Global variable to store the compiled WebAssembly module Promise
+let talkPromise: Promise<WebAssembly.Module> | null = null;
 
 // Function to initialize WebAssembly module
 async function initTalk(): Promise<WebAssembly.Module> {
-  if (talk === null) {
-    // Initialize WASI if not done yet
-    await init();
+  if (!talkPromise) {
+    talkPromise = (async () => {
+      // Initialize WASI if not done yet
+      await init();
 
-    // Load the .wasm from the public dir
-    const talkresponse = await fetch("/talk.wasm.gzip");
+      // Load the .wasm from the public dir
+      const talkresponse = await fetch("/talk.wasm.gzip");
 
-    const responseBlob = await talkresponse.blob();
-    const decompressionStream = new DecompressionStream("gzip");
-    const decompressedArrayBuffer = await new Response(
-      responseBlob.stream().pipeThrough(decompressionStream)
-    ).arrayBuffer();
+      const responseBlob = await talkresponse.blob();
+      const decompressionStream = new DecompressionStream("gzip");
+      const decompressedArrayBuffer = await new Response(
+        responseBlob.stream().pipeThrough(decompressionStream)
+      ).arrayBuffer();
 
-    const talkbin = new Uint8Array(decompressedArrayBuffer);
-    const talklowered = await lowerI64Imports(talkbin);
+      const talkbin = new Uint8Array(decompressedArrayBuffer);
+      const talklowered = await lowerI64Imports(talkbin);
 
-    // Compile the WebAssembly module and store it globally
-    talk = await WebAssembly.compile(talklowered);
+      // Compile the WebAssembly module and return it
+      return WebAssembly.compile(talklowered);
+    })();
   }
 
-  return talk;
+  return talkPromise;
 }
 
 export async function execute(
